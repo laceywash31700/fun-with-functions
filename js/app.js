39e91115ac37b9a570289701cc84360486a63574
lacey washington <lacey.m.washington@gmail.com>
2023-02-18 16:36:45 -0600

    updated branch 2

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ function multiply(a, b) {
   let string =`The product of ${a} and ${b} is ${product}.`;
   return [product,string];
 
-}
+}     
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
